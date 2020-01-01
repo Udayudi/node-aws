81a4a8698373582c41796201997f752725d2a9bc
Udayudi <udaygam@outlook.com>
2020-01-01 20:34:32 +0530

    New Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+    return res.send('FROM AWS');
+}); 
+
+
+app.listen(port, () => {
+    console.log(`Server started on port`);
+});
